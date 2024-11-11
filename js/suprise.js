58d92aeb5829b5c23368ed5e6b7b30cb246959ce
Ani <dzaficanida@gmail.com>
2024-11-11 22:31:45 +0100

    Add files via upload

---
+++ js/suprise.js
@@ -0,0 +1,8 @@
+document.querySelector('.button-suprise').addEventListener('click', function() {
+    Swal.fire({
+        title: 'Credits Time!',
+        text: 'This site was made by Anida Džafić.',
+        icon: 'info',
+        confirmButtonText: 'BYE'
+    });
+});
